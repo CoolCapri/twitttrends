c6e3ea926713199e387c85c3b3189d236062e3d5
Shuo Wang <coolcaprimail@163.com>
2016-11-12 23:10:49 -0500

    update index.html to look for resoruces using url_for

--- es/esearch.py
+++ es/esearch.py
@@ -5,7 +5,7 @@ import requests
 class ESearch():
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     def search(self, keyword):
-        es_data = es.search(index="tweet", size=2000, body={"query": {"match": {'text':{'query': keyword}}}})
+        es_data = self.es.search(index="tweet", size=2000, body={"query": {"match": {'text':{'query': keyword}}}})
         es_results = es_data['hits']['hits']
         tweets = []
         for es_result in es_results:
